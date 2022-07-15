111cf5df8d162418922c1270a076285bd2cfc5dd
GuillaumeBrthlt <bertholetguillaume@gmail.com>
2022-07-15 19:54:58 +0200

    ajout des exercices Play with Ruby

---
+++ create_ruby.rb
@@ -0,0 +1,51 @@
+# Va créer un dossier RUBY
+
+def check_if_input
+  abort("mkdir missing input") if ARGV empty?
+end
+
+def get_folder_name
+  return folder_name = ARGV.first
+end
+
+def create_directory(folder_name)
+  Dir.mkdir(folder_name)
+end
+
+def create__sub_directories(folder_name)
+  Dir.mkdir("#{folder_name}/lib")
+  Dir.mkdir("#{folder_name}/spec")
+end
+
+def create_gemfile
+  file = File.open("Gemfile", "w")
+  file.puts("source 'https://rubygems.org'")
+  file.puts("ruby '2.7.4'")
+  file.puts("gem 'rspec'")
+  file.puts("gem 'pry'")
+  file.puts("gem 'rubocop'")
+  file.puts("gem 'dotenv'")
+  file.close
+end
+
+def create_env
+  file = File.open(".env", "w")
+  file.close
+end
+
+def init_git_rspec(folder_name)
+  Dir.chdir("#{folder_name}")
+  system("rspec --init")
+  system("git init")
+end
+
+def perform
+  folder_name = get_folder_name
+  create_directory(folder_name)
+  create__sub_directories(folder_name)
+  init_git_rspec(folder_name)
+  create_gemfile
+  create_env
+end
+
+perform
---
+++ google_searcher.rb
@@ -0,0 +1,17 @@
+require 'watir'
+
+browser_opts = {detach: true}
+browser = Watir::Browser.new:chrome, options: browser_opts
+browser.goto 'https://www.google.com/?gws_rd=ssl'
+
+research = ARGV.first
+
+accept_button = browser.button(id: 'L2AGLb')
+
+accept_button.click
+
+search_bar = browser.text_field(class: 'gsfi')
+
+search_bar.set(research)
+
+search_bar.send_keys(:enter)
